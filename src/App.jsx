4b9a91efc728f8847472044f2ec30a66ca0fbd25
nde booy <ndesimoniche@gmail.com>
2024-09-05 00:35:38 +0400

    almost done with setting up the project

---
+++ src/App.jsx
@@ -0,0 +1,15 @@
+import styled from "styled-components";
+
+const H1 = styled.h1({
+  fontSize: "x-large",
+});
+
+const App = () => {
+  return (
+    <div>
+      <H1>hello world</H1>
+    </div>
+  );
+};
+
+export default App;
